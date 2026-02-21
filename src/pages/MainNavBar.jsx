d188349b018803ff2cc13ca28f8cf0e3158ac8dc
gayathri <2500031821@kluniversity.in>
2026-02-21 11:00:35 +0530

    project v1.0

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -16,7 +16,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-           KL Event Management System
+           KL Event Management System -2500031821
           </Link>
         </div>
         <ul className="nav-menu">
